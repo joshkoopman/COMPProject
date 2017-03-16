ebcc7cbab4397b30869fb5ad172792bfc2f03c6b
joshkoopman <jkoopman@prospera.ca>
2017-03-16 15:12:16 -0700

    Initial commit

---
+++ src/main/java/com/example/jkoopman/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.jkoopman.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button lbutton = (Button) findViewById(R.id.launch_button);
+        lbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MathActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        final Button cbutton = (Button) findViewById(R.id.checkScore_button);
+        cbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+    }
+}
